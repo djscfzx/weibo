88b437f23e1a7eb7d1584877e67471f61e42f160
vagrant <vagrant@homestead>
2019-03-01 20:25:39 +0000

    独立头部和底部视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551481582007&di=a28efb77d105a052bf6aa5e0dab14dde&imgtype=0&src=http%3A%2F%2Fwx1.sinaimg.cn%2Fthumb150%2F005EYogEgy1ftjuaqebwkg300k00kdfl.gif">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    _(:з」∠)_
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
