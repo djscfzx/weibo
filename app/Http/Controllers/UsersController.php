38de6c8c448f1e908fb77ec1541400fa635d2c3c
vagrant <vagrant@homestead>
2019-03-05 13:21:50 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,25 +11,47 @@ class UsersController extends Controller {
     return view('users.create');
   }
 
+  public function edit(User $user) {
+    return view('users.edit', compact('user'));
+  }
+
   public function show(User $user) {
     return view('users.show', compact('user'));
   }
 
   public function store(Request $request) {
     $this->validate($request, [
-        'name' => 'required|min:3|max:50',
-        'email' => 'required|email|unique:users|max:255',
-        'password' => 'required|confirmed|min:6'
+      'name' => 'required|min:3|max:50',
+      'email' => 'required|email|unique:users|max:255',
+      'password' => 'required|confirmed|min:6'
     ]);
 
     $user = User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => bcrypt($request->password),
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => bcrypt($request->password),
     ]);
 
     Auth::login($user);
     session()->flash('success', '欢迎，这只是个测试APP请勿当真');
     return redirect()->route('users.show', [$user]);
+  }
+
+  public function update(User $user, Request $request) {
+    $this->validate($request, [
+      'name' => 'required|max:50',
+      'password' => 'nullable|confirmed|min:6'
+    ]);
+
+    $data = [];
+    $data['name'] = $request->name;
+    if ($request->password) {
+      $data['password'] = bcrypt($request->password);
+    }
+    $user->update($data);
+
+    session()->flash('success', '更新成功');
+
+    return redirect()->route('users.show', $user);
   }
 }
