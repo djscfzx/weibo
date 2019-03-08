7a6755e854ceb4455204982923a46542a67f3ec2
vagrant <vagrant@homestead>
2019-03-08 19:17:02 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,16 +1,31 @@
 @extends('layouts.default')
 
 @section('content')
-  <div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p class="lead">
-      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel</a> 的示例项目。
-    </p>
-    <p>
-      这是第一个应用。
-    </p>
-    <p>
-      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">注册</a>
-    </p>
-  </div>
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel</a> 的示例项目
+      </p>
+      <p>
+        JUST DONE IT
+      </p>
+      <p>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">注册</a>
+      </p>
+    </div>
+  @endif
 @stop
