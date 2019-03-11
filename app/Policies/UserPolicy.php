98b425700626d6e413e8072846e70743b3dc486e
vagrant <vagrant@homestead>
2019-03-11 17:07:52 +0000

    关注按钮

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -6,11 +6,17 @@ use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class UserPolicy {
-    use HandlesAuthorization;
-    public function update(User $currentUser, User $user) {
-      return $currentUser->id === $user->id;
-    }
-    public function destroy(User $currentUser, User $user) {
-      return $currentUser->is_admin && $currentUser->id !== $user->id;
-    }
+  use HandlesAuthorization;
+
+  public function update(User $currentUser, User $user) {
+    return $currentUser->id === $user->id;
+  }
+
+  public function destroy(User $currentUser, User $user) {
+    return $currentUser->is_admin && $currentUser->id !== $user->id;
+  }
+
+  public function follow(User $currentUser, User $user) {
+    return $currentUser->id !== $user->id;
+  }
 }
