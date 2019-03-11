42ee3adf8c4ac0b4af6eb14d6f7dbe62b911ed3c
vagrant <vagrant@homestead>
2019-03-11 16:26:46 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,66 +9,79 @@ use App\Notifications\ResetPassword;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    public function statuses() {
-        return $this->hasMany(Status::class);
+  public function statuses() {
+    return $this->hasMany(Status::class);
+  }
+
+  /*粉丝*/
+  public function followers() {
+    return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+  }
+
+  /*关注*/
+  public function followings() {
+    return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+  }
+
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
+
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
+
+  public static function boot() {
+    parent::boot();
+
+    static::creating(function ($user) {
+      $user->activation_token = str_random(30);
+    });
+  }
+
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
+  /**
+   * 『 生成头像 』
+   * 为 gravatar 方法传递的参数 size 指定了默认值 100；
+   * 通过 $this->attributes['email'] 获取到用户的邮箱；
+   * 使用 trim 方法剔除邮箱的前后空白内容；
+   * 用 strtolower 方法将邮箱转换为小写；
+   * 将小写的邮箱使用 md5 方法进行转码；
+   * 将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；
+   */
+  public function gravatar($size = '100') {
+    $hash = md5(strtolower(trim($this->attributes['email'])));
+    return "http://www.gravatar.com/avatar/$hash?s=$size";
+  }
+
+  public function sendPasswordResetNotification($token) {
+    $this->notify(new ResetPassword($token));
+  }
+
+  public function feed() {
+    return $this->statuses()->orderBy('created_at', 'desc');
+  }
+
+  public function follow($user_ids) {
+    if ( ! is_array($user_ids)) {
+        $user_ids = compact('user_ids');
     }
+    $this->followings()->sync($user_ids, false);
+  }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    public static function boot() {
-        parent::boot();
-
-        static::creating(function ($user) {
-            $user->activation_token = str_random(30);
-        });
+  public function unfollow($user_ids) {
+    if ( ! is_array($user_ids)) {
+        $user_ids = compact('user_ids');
     }
+    $this->followings()->detach($user_ids);
+  }
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    /**
-     * 『 生成头像 』
-     * 为 gravatar 方法传递的参数 size 指定了默认值 100；
-     * 通过 $this->attributes['email'] 获取到用户的邮箱；
-     * 使用 trim 方法剔除邮箱的前后空白内容；
-     * 用 strtolower 方法将邮箱转换为小写；
-     * 将小写的邮箱使用 md5 方法进行转码；
-     * 将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；
-     */
-    public function gravatar($size = '100') {
-        $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "http://www.gravatar.com/avatar/$hash?s=$size";
-    }
-
-    public function sendPasswordResetNotification($token) {
-        $this->notify(new ResetPassword($token));
-    }
-
-    public function feed() {
-        return $this->statuses()->orderBy('created_at', 'desc');
-    }
+  public function isFollowing($user_id) {
+    return $this->followings->contains($user_id);
+  }
 }
